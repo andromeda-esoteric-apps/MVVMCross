1b09659c3d50ccfa0b039eafc65a8601ed1668f5
Antonio Acevedo <tonio_acevedo@hotmail.com>
2020-05-07 19:29:40 -0500

    Se adiciona cascaron de proyecto IOS, ya que se requiere un Mac para poder continuar.

---
+++ TipCalculator.Ios/AppDelegate.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using TipCalculator.Core;
+using UIKit;
+
+namespace TipCalculator.Ios
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+
+    }
+
+}
+
+
